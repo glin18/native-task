c3ef71a8bd43bc25620057a2a7e191b8f9581535
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-02 23:00:29 +0800

    Begin creating home page cards

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,17 @@
-import { SafeAreaView, View, Text, Image } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
-      <View className="flex-row">
+      <View className="flex-row justify-around mt-5 items-center px-2">
         <View className="p-6">
           <Text className="text-4xl font-bold">Hello Gary</Text>
           <Text>Today you have 4 tasks </Text>
@@ -14,6 +21,28 @@ const HomeScreen = () => {
           style={{ height: 80, width: 80, resizeMode: "contain" }}
         />
       </View>
+      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+        <View className="bg-white w-64 h-24 shadow-md rounded-lg mt-4">
+          <Text>Today</Text>
+          <Text>4 Tasks</Text>
+        </View>
+        <View className="bg-white w-64 h-24 shadow-md rounded-lg mt-4">
+          <Text>Today</Text>
+          <Text>4 Tasks</Text>
+        </View>
+        <View className="bg-white w-64 h-24 shadow-md rounded-lg mt-4">
+          <Text>Today</Text>
+          <Text>4 Tasks</Text>
+        </View>
+        <View className="bg-white w-64 h-24 shadow-md rounded-md mt-4">
+          <Text>Today</Text>
+          <Text>4 Tasks</Text>
+        </View>
+        <View className="bg-white w-64 h-24 shadow-md rounded-lg mt-4">
+          <Text>Today</Text>
+          <Text>4 Tasks</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
