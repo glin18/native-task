c77ed5d3e58df0d7ee388610e91d5847a41917fb
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-03 07:39:43 +0800

    Move homescreen to screens directory

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./components/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
