305780dd6a2efe9a486aeb5c3205e4473181f8b3
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-04 23:09:46 +0800

    Add onAuthStateChanged useeffect

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,10 +1,21 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import { auth } from "../firebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(auth?.currentUser?.email);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(auth?.currentUser?.email);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+  }, []);
 
   return (
     <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
