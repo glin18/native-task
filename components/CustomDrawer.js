3d33184905f79109dbbf1381e245df1125862204
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-05 13:38:50 +0800

    Add signout functionality

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -5,11 +5,27 @@ import {
   DrawerItemList,
 } from "@react-navigation/drawer";
 import { ArrowRightOnRectangleIcon } from "react-native-heroicons/outline";
+import { auth } from "../firebaseConfig";
+import { useAuth } from "../hooks/useAuth";
 
 const CustomDrawer = (props) => {
+  const { setCurrentUser } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      setCurrentUser(null);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <SafeAreaView className="flex-1">
-      <TouchableOpacity className="flex-row gap-x-4 items-center pl-6 mt-4">
+      <TouchableOpacity
+        onPress={handleSignOut}
+        className="flex-row gap-x-4 items-center pl-6 mt-4"
+      >
         <ArrowRightOnRectangleIcon color="black" size={28} />
         <Text className="font-bold text-xl">Log Out</Text>
       </TouchableOpacity>
