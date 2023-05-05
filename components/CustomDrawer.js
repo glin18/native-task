ae89ba0753445954633d95ec0093e5620c3a11a2
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-05 14:03:35 +0800

    Add customize button and email

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -4,7 +4,11 @@ import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
-import { ArrowRightOnRectangleIcon } from "react-native-heroicons/outline";
+import {
+  ArrowRightOnRectangleIcon,
+  Cog6ToothIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
 import { auth } from "../firebaseConfig";
 import { useAuth } from "../hooks/useAuth";
 
@@ -22,6 +26,21 @@ const CustomDrawer = (props) => {
 
   return (
     <SafeAreaView className="flex-1">
+      <View className="pl-6 mt-1 flex-row items-center gap-x-4">
+        <UserIcon color="black" size={28} />
+        <Text className="font-bold text-lg">{auth?.currentUser?.email}</Text>
+      </View>
+      <View
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: 1,
+          margin: 20,
+        }}
+      />
+      <TouchableOpacity className="flex-row gap-x-4 items-center pl-6 mt-4">
+        <Cog6ToothIcon color="black" size={28} />
+        <Text className="font-bold text-xl">Customize</Text>
+      </TouchableOpacity>
       <TouchableOpacity
         onPress={handleSignOut}
         className="flex-row gap-x-4 items-center pl-6 mt-4"
