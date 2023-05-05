ff928ce3c054557993de0886848102eab1a8a789
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-05 13:26:54 +0800

    Add logout button

--- components/CustomDrawer.js
+++ components/CustomDrawer.js
@@ -1,17 +1,22 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from "@react-navigation/drawer";
+import { ArrowRightOnRectangleIcon } from "react-native-heroicons/outline";
 
 const CustomDrawer = (props) => {
   return (
-    <View style={{ flex: 1 }}>
+    <SafeAreaView className="flex-1">
+      <TouchableOpacity className="flex-row gap-x-4 items-center pl-6 mt-4">
+        <ArrowRightOnRectangleIcon color="black" size={28} />
+        <Text className="font-bold text-xl">Log Out</Text>
+      </TouchableOpacity>
       {/* <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
       </DrawerContentScrollView> */}
-    </View>
+    </SafeAreaView>
   );
 };
 
