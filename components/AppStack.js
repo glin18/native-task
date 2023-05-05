f7f6faa6c6ac213635ce94cba7ac809150181cd9
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-05 12:15:17 +0800

    Add taskscreen to drawers

--- components/AppStack.js
+++ components/AppStack.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
+import TaskScreen from "../screens/TaskScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,6 +13,12 @@ const AppStack = () => {
         name="Home"
         component={HomeScreen}
         options={{ headerShown: false }}
+      />
+      <Drawer.Screen
+        name="Task"
+        component={TaskScreen}
+        options={{ headerShown: false }}
+        initialParams={{ category: "Error", numTasks: "0" }}
       />
     </Drawer.Navigator>
   );
