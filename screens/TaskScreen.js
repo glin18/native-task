a02a2ee143ebcbd9cf6b2234dca2f0c01c9a20df
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-06 15:53:23 +0800

    Add add task modal to task screen

--- screens/TaskScreen.js
+++ screens/TaskScreen.js
@@ -5,42 +5,24 @@ import {
   Text,
   ScrollView,
   TouchableOpacity,
+  Modal,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import {
   ArrowLongLeftIcon,
   EllipsisVerticalIcon,
+  XCircleIcon,
 } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
 import Todo from "../components/Todo";
 import { Icon } from "@rneui/themed";
-
-// const DATA = [
-//   {
-//     id: "1",
-//     todo: "First Item",
-//     isCompleted: false,
-//   },
-//   {
-//     id: "2",
-//     todo: "Second Item",
-//     isCompleted: false,
-//   },
-//   {
-//     id: "3",
-//     todo: "Third Item",
-//     isCompleted: false,
-//   },
-//   {
-//     id: "4",
-//     todo: "Fourth Item",
-//     isCompleted: false,
-//   },
-// ];
+import { set } from "react-native-reanimated";
 
 const TaskScreen = ({ route }) => {
   const navigator = useNavigation();
   const { category, tasks } = route.params;
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <SafeAreaView className="h-full">
       <View className="flex-row justify-between px-6">
@@ -71,7 +53,23 @@ const TaskScreen = ({ route }) => {
           )}
         />
       </View>
-      <TouchableOpacity>
+      <Modal animationType="slide" transparent={true} visible={modalVisible}>
+        <SafeAreaView className="bg-slate-400 relative flex-1 mt-44">
+          <View className="mt-10">
+            <View className="flex-row justify-center align-center">
+              <Text className="text-3xl">Add Task</Text>
+            </View>
+            <TouchableOpacity className="absolute right-10">
+              <XCircleIcon
+                color="black"
+                size={40}
+                onPress={() => setModalVisible(false)}
+              />
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </Modal>
+      <TouchableOpacity onPress={() => setModalVisible(true)}>
         <Icon name="add-circle-outline" type="ionicon" size={60} />
       </TouchableOpacity>
     </SafeAreaView>
