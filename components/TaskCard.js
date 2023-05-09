41398170e10c21abf6a11446548f143bd91ff430
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-09 13:27:17 +0800

    Add trash icon to customize card

--- components/TaskCard.js
+++ components/TaskCard.js
@@ -1,17 +1,27 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import { TrashIcon } from "react-native-heroicons/outline";
 
 const TaskCard = ({ category, disable = false }) => {
   const navigator = useNavigation();
 
   if (disable) {
     return (
-      <View className="bg-white w-72 h-24 shadow-md rounded-lg mt-4 flex-col justify-center items-center">
-        <Text className="font-bold text-xl">{category.data.name}</Text>
-        <Text className="text-color-gray">
-          {category.data.tasks.length} Tasks
-        </Text>
+      <View className="bg-white w-72 h-24 shadow-md rounded-lg mt-4 flex-row justify-between items-center">
+        <View style={{ opacity: 0 }}>
+          <TrashIcon />
+        </View>
+        <View className="justify-center items-center pl-3">
+          <Text className="font-bold text-xl">{category.data.name}</Text>
+          <Text className="text-color-gray">
+            {category.data.tasks.length} Tasks
+          </Text>
+        </View>
+
+        <View className="pr-3">
+          <TrashIcon />
+        </View>
       </View>
     );
   }
